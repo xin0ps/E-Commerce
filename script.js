dfd8d360b89ccf1a5b9aa3e478736141b0bb7f80
Rasul Aslanov <rasulsha@code.edu.az>
2024-06-02 02:24:29 +0400

    15

--- script.js
+++ script.js
@@ -179,6 +179,20 @@ else{
     productDetails.style.zIndex = "99"
 });
 
+function az(productId) {
+  var counterValue = parseInt($("#counter-" + productId).text()); 
+  if (counterValue > 1) {
+      counterValue--; 
+      $("#counter-" + productId).text(counterValue); 
+  }
+}
+
+function art(productId) {
+  var counterValue = parseInt($("#counter-" + productId).text()); 
+  counterValue++; 
+  $("#counter-" + productId).text(counterValue); 
+}
+  
 
 
 $("#orders").click(() => {
@@ -188,7 +202,7 @@ $("#orders").click(() => {
       
       
       const cartItemDiv = `
-      <div class="cart-item mt-[10px] flex flex-col gap-3 ">
+      <div class="cart-item mt-[10px] flex flex-col gap-4 ">
       <div id="product-images" class="flex overflow-x-auto w-full px-10 gap-2 border-1 border-grey">
           ${selectedProduct.gallery.map(image => `
       
@@ -197,6 +211,15 @@ $("#orders").click(() => {
                   </div>`).join('')}
  
           </div>
+
+          <div class="flex justify-between items-center w-full">
+          <div class="flex items-center  gap-10">
+              <button onclick="az(${orderProduct.id})" class="bg-gray-200 border-2 border-[black] w-8 h-8 flex justify-center items-center last">-</button>
+              <p id="counter-${orderProduct.id}" class="font-bold">1</p>
+              <button onclick="art(${orderProduct.id})" class="bg-gray-200 border-2 border-[black] w-8 h-8 flex justify-center items-center last">+</button>
+          </div>
+      </div>
+
           <p id="product-brand">${selectedProduct.brand}</p>
           <p id="product-title">${selectedProduct.title}</p>
           <div id="sizediv" class="flex font-bold font-sans text-sm gap-2">SIZE:${sizeDivContent}</div>
@@ -205,6 +228,7 @@ $("#orders").click(() => {
       </div>
   `;
       
+  
       
       $("#cart").append(cartItemDiv)
     });
@@ -227,7 +251,15 @@ $("#orders").click(() => {
     cart.style.left = "50%";
     cart.style.transform = "translate(-50%, -60%)";
     cart.style.zIndex = "99";
+
+    
+   
 });
+
+
+
+
+
 
 $("#addtocart").click(()=>{
 order.push(orderProduct);
